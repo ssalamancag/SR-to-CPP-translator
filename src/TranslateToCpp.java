225f59af348f61b0515e784bdf466b96fc2fd7c6
Cesar Torres <ceatorresar@unal.edu.co>
2019-11-12 07:29:27 -0500

    Creación de estructua C++

--- src/TranslateToCpp.java
+++ src/TranslateToCpp.java
@@ -4,19 +4,35 @@ import org.antlr.v4.runtime.tree.TerminalNode;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class TranslateToCpp extends MiLenguajeBaseListener {
 
+    // Flujo de escritura en el archivo
     BufferedWriter br;
 
-    public TranslateToCpp() {
+    // Estructura de C++
+    ArrayList<String> imports;      // Lineas de Importaciones
+    String namespace;               // Linea Namespace
+    ArrayList<String> globals;      // Lista variables globales
+    ArrayList<String> functions;    // Lista funciones
+    String main;                    // Función principal
 
+    // Constructor
+    public TranslateToCpp() {
         try {
 
             // Crear objeto de escritura
             FileWriter writer = new FileWriter("output/salida.txt");
             // Crear objeto de escritura directa
             br = new BufferedWriter(writer);
+
+            // Instanciar estructura C++
+            imports = new ArrayList<>();
+            namespace = "using namespace std;";
+            globals = new ArrayList<String>();
+            functions = new ArrayList<String>();
+            main = "int main() {\n\t";
 
         } catch (IOException ex) {
             System.out.println(ex);
@@ -27,16 +43,34 @@ public class TranslateToCpp extends MiLenguajeBaseListener {
     // Escribir en archivo
     public void write(String text){
         try {
+
+            // Escribir importaciones
+            for (String imprt: imports) {
+                br.write("\n" + imprt);
+            }
             br.write(text);
+
+            // Escribir namespace
+            br.write("\n" + namespace);
+
+            // Escribir variables globales
+            for (String global: globals) {
+                br.write("\n" + global);
+            }
+            br.write(text);
+
+            // Escribir funciones
+            for (String function: functions) {
+                br.write("\n" + function);
+            }
+            br.write(text);
+
+            // Escribir Main
+            br.write("\n" + namespace);
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    // Al visitar terminal
-    @Override public void visitTerminal(TerminalNode node) {
-        write(node.getSymbol().getText());
-    }
-
-
+    
 }
